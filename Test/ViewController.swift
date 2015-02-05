aa570dc4653f82839c312c7339400780e31a6cd2
Fadie Hannona <fadiehannona@gmail.com>
2015-02-05 05:16:00 +0000

    receiving offers back from server

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
+import AlamofireSwiftyJSON
 
 class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet var tableView:UITableView!
     
     let getJson = "http://ec2-54-69-64-196.us-west-2.compute.amazonaws.com:3000/geocode/"
     let postJson = "http://ec2-54-69-64-196.us-west-2.compute.amazonaws.com:3000/offers/"
     var city = ""
-    var latitudeString = ""
-    var longitudeString = ""
-    var postParameters = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,40 +31,46 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
     func searchBar(searchBar: UISearchBar,
         textDidChange searchText: String) {
-            
+    
     }
     
-    func getJsonFromServer(city: String) {
+    
+    func getJson(city: String) {
         var getJsonURL = getJson+city
         
         Alamofire.request(.GET, getJsonURL).responseJSON {
-            (request, response, jsonData, error) in
-            if error != nil {
-                println(error)
-            } else if let json = jsonData as? [String:AnyObject] {
-                println(json)
-                println(jsonData)
-                self.processJsonData(json)
-            } else {
-                println("Failed to cast JSON to [String:AnyObject]")
+            (request, response, json, error) in
+            if (json != nil) {
+                var jsonObj = JSON(json!)
+                var latitudeString = jsonObj["latitude"].stringValue
+                var longitudeString = jsonObj["longitude"].stringValue
+                self.postJson(latitudeString, longitudeString: longitudeString)
             }
         }
     }
     
-    func processJsonData(json: [String:AnyObject]) {
-        if let str = json["longitude"] as? String {
-            latitudeString = str
-            println(latitudeString)
-        }
-    }
-    
-    func postJsonToServer() {
+    func postJson(latitudeString: AnyObject, longitudeString: AnyObject) {
+        
         var postJsonURL = postJson+city
+        var postParameters : [ String : AnyObject] = [
+            "latitude": latitudeString,
+            "longitude": longitudeString,
+            "offset": 0
+        ]
+        
+        Alamofire.request(.POST, postJsonURL, parameters: postParameters, encoding: .JSON).responseJSON {
+            (request, response, json, error) -> Void in
+            println(json!)
+        }
+        
+        
     }
+
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
         searchBar.delegate = self;
         var city = searchBar.text
-        self.getJsonFromServer(city)
+        self.getJson(city)
     }
+    
 }
